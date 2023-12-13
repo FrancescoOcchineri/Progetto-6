108502a9bbe7450706e380b4ea50dc5cb505fae3
FrancescoOcchineri <occhinerifrancesco@gmail.com>
2023-12-13 18:34:11 +0100

    Modifica 50%

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,45 +1,93 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let name = document.querySelector('#name').value
-    let description = document.querySelector('#category').value
-    let brand = document.querySelector('.form-select').value
-    let img = document.querySelector('#img').value
-    let price = document.querySelector('#price').value
-    let product = {
-        name: name,
-        description: description,
-        brand: brand,
-        img: img,
-        price: price
-    }
-
+document.addEventListener("DOMContentLoaded", () => {
+    let button = document.querySelector('.invia');
     let url = "https://striveschool-api.herokuapp.com/api/product/";
 
     function getData(url) {
         fetch(url, {
             method: 'GET',
             headers: {
-                Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTc4MzU0OWMwNTgzNTAwMTg1MjJmODgiLCJpYXQiOjE3MDIzNzY3NzcsImV4cCI6MTcwMzU4NjM3N30.vNStGYea9QyMEVDBApMNADTJtNOZ7J0Hg6Z4ldzcaXQ'
+                Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTc4YWE0MDI2NzYxNDAwMTgzYzNiZDciLCJpYXQiOjE3MDI0MDY3MjUsImV4cCI6MTcwMzYxNjMyNX0.SaYsGxqmoTxMxi02CtqyfzZMi3PNHMeQG3ZlsBOHhn0"
             }
         })
             .then(response => response.json())
             .then(json => {
                 console.log(json);
-
+                json.forEach(p => {
+                    console.log(p._id)
+                })
             })
             .catch(error => console.error('Errore durante la richiesta:', error));
     }
-    getData(url)
-    createData(product, url)
+    getData(url);
 
-    function createData(product, url) {
+    function createData(json, url) {
         fetch(url, {
             method: "POST",
-            body: JSON.stringify(product),
+            body: JSON.stringify(json),
             headers: {
-                Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTc4MzU0OWMwNTgzNTAwMTg1MjJmODgiLCJpYXQiOjE3MDIzNzY3NzcsImV4cCI6MTcwMzU4NjM3N30.vNStGYea9QyMEVDBApMNADTJtNOZ7J0Hg6Z4ldzcaXQ',
+                Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTc4YWE0MDI2NzYxNDAwMTgzYzNiZDciLCJpYXQiOjE3MDI0MDY3MjUsImV4cCI6MTcwMzYxNjMyNX0.SaYsGxqmoTxMxi02CtqyfzZMi3PNHMeQG3ZlsBOHhn0",
                 "Content-Type": "application/json"
             }
         })
+            .then(response => response.json())
+            .then(json => {
+                console.log(json)
+            })
             .catch(err => console.log(err))
     }
+
+    function deleteProduct() {
+        let productId = document.querySelector('#product-id-input').value;
+        fetch(`https://striveschool-api.herokuapp.com/api/product/${productId}`, {
+            method: 'DELETE',
+            headers: {
+                Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTc4YWE0MDI2NzYxNDAwMTgzYzNiZDciLCJpYXQiOjE3MDI0MDY3MjUsImV4cCI6MTcwMzYxNjMyNX0.SaYsGxqmoTxMxi02CtqyfzZMi3PNHMeQG3ZlsBOHhn0",
+                "Content-Type": "application/json"
+            }
+
+        })
+            .then(response => {
+                if (response.ok) {
+                    fetchProducts();
+                } else {
+                    console.error('Errore durante la richiesta DELETE:', response.statusText);
+                }
+            })
+            .catch(error => console.error('Errore durante la richiesta DELETE:', error));
+    }
+
+    button.addEventListener('click', () => {
+        let name = document.querySelector('#name').value
+        let description = document.querySelector('#category').value
+        let brand = document.querySelector('.form-select').value
+        let img = document.querySelector('#img').value
+        let price = document.querySelector('#price').value
+        let product = {
+            name: name,
+            description: description,
+            brand: brand,
+            imageUrl: img,
+            price: price
+        }
+        createData(product, url)
+        let inputControl = document.querySelector('.controllo')
+        if ((inputControl.value == "") || (inputControl == "undefined")) {
+            alert("Il campo Nome è obbligatorio.");
+            document.modulo.nome.focus();
+            return false;
+        }
+    })
+    let back = document.querySelector('.back')
+    back.addEventListener('click', () => {
+        window.location = 'home.html'
+    })
+    let home = document.querySelector('.home')
+    home.addEventListener('click', () => {
+        window.location = 'home.html'
+    })
+    let cancella = document.querySelector('.cancella');
+    cancella.addEventListener('click', () => {
+        deleteProduct();
+    })
+
 })
--- assets/js/script.js
+++ assets/js/script.js
@@ -9,14 +9,14 @@ class Product {
 }
 
 let p1 = new Product("Diablo IV", "Open-world RPG", "Blizzard Entertainment", "https://m.media-amazon.com/images/I/61SrkfhCyCL._AC_SL1400_.jpg", 79.99);
-let p2 = new Product("The Witcher 3", "Action RPG", "CD Projekt", "https://m.media-amazon.com/images/I/81FK7ZyBvTL._AC_SL1500_.jpg", 49.99);
+let p2 = new Product("The Witcher 3", "Action RPG", "CD Projekt", "https://m.media-amazon.com/images/I/81AeDNt4STL._AC_SL1500_.jpg", 49.99);
 let p3 = new Product("Cyberpunk 2077", "Open-world RPG", "CD Projekt", "https://m.media-amazon.com/images/I/81ciqPxpHEL._AC_SL1500_.jpg", 59.99);
-let p4 = new Product("Red Dead Redemption 2", "Action-Adventure", "Rockstar Games", "https://m.media-amazon.com/images/I/71az6dtuJxL._AC_SL1500_.jpg", 39.99);
-let p5 = new Product("Assassin's Creed Valhalla", "Action RPG", "Ubisoft", "https://m.media-amazon.com/images/I/81FgG8d4P0L._AC_SL1500_.jpg", 69.99);
-let p6 = new Product("Final Fantasy XV", "Action RPG", "Square Enix", "https://m.media-amazon.com/images/I/91gOxtYdu9L._AC_SL1500_.jpg", 49.99);
-let p7 = new Product("Ghost of Tsushima", "Action-Adventure", "Sucker Punch Productions", "https://m.media-amazon.com/images/I/81cIKu20eKL._AC_SL1500_.jpg", 59.99);
-let p8 = new Product("Horizon Zero Dawn", "Action RPG", "Guerrilla Games", "https://m.media-amazon.com/images/I/61Cn9b-2YfL._AC_SL1500_.jpg", 39.99);
-let p9 = new Product("Elder Scrolls V: Skyrim", "Action RPG", "Bethesda Game Studios", "https://m.media-amazon.com/images/I/71zwAO5o5rL._AC_SL1500_.jpg", 29.99);
+let p4 = new Product("Red Dead Redemption 2", "Action-Adventure", "Rockstar Games", "https://m.media-amazon.com/images/I/81GEQY7xeAL._AC_SL1500_.jpg", 39.99);
+let p5 = new Product("Assassin's Creed Valhalla", "Action RPG", "Ubisoft", "https://m.media-amazon.com/images/I/71t3-jTtScL._AC_SL1500_.jpg", 69.99);
+let p6 = new Product("Final Fantasy XV", "Action RPG", "Square Enix", "https://m.media-amazon.com/images/I/71CNs8FaCAL._AC_SL1500_.jpg", 49.99);
+let p7 = new Product("Ghost of Tsushima", "Action-Adventure", "Sucker Punch Productions", "https://m.media-amazon.com/images/I/81R1L3xzD1S._AC_SL1500_.jpg", 59.99);
+let p8 = new Product("Horizon Zero Dawn", "Action RPG", "Guerrilla Games", "https://m.media-amazon.com/images/I/81DXz-lWkSL._AC_SL1500_.jpg", 39.99);
+let p9 = new Product("Elder Scrolls V: Skyrim", "Action RPG", "Bethesda Game Studios", "https://m.media-amazon.com/images/I/81WP+qz1eTL._AC_SL1500_.jpg", 29.99);
 
 document.addEventListener("DOMContentLoaded", async () => {
     getData(url);
@@ -28,31 +28,96 @@ function getData(url) {
     fetch(url, {
         method: 'GET',
         headers: {
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTc4MzU0OWMwNTgzNTAwMTg1MjJmODgiLCJpYXQiOjE3MDIzNzY3NzcsImV4cCI6MTcwMzU4NjM3N30.vNStGYea9QyMEVDBApMNADTJtNOZ7J0Hg6Z4ldzcaXQ'
+            Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTc4YWE0MDI2NzYxNDAwMTgzYzNiZDciLCJpYXQiOjE3MDI0MDY3MjUsImV4cCI6MTcwMzYxNjMyNX0.SaYsGxqmoTxMxi02CtqyfzZMi3PNHMeQG3ZlsBOHhn0"
         }
     })
         .then(response => response.json())
         .then(json => {
             console.log(json)
-            let img = document.querySelectorAll('img');
-            img.forEach(image => {
-                image.src = json[0].imageUrl
-            })
+            let cardImages = document.querySelectorAll('.card img');
+            for (let i = 0; i < cardImages.length; i++) {
+                cardImages[i].src = json[i].imageUrl
+            }
+            let cardTitle = document.querySelectorAll('.card-title');
+            for (let i = 0; i < cardTitle.length; i++) {
+                cardTitle[i].innerHTML = json[i].name
+            }
+            let cardDescription = document.querySelectorAll('.description');
+            for (let i = 0; i < cardDescription.length; i++) {
+                cardDescription[i].innerHTML = json[i].description
+            }
+            let cardBrand = document.querySelectorAll('.brand');
+            for (let i = 0; i < cardBrand.length; i++) {
+                cardBrand[i].innerHTML = json[i].brand
+            }
+            let cardPrice = document.querySelectorAll('.price');
+            for (let i = 0; i < cardPrice.length; i++) {
+                cardPrice[i].innerHTML = json[i].price + '€'
+            }
+            for (let i = 9; i < json.length; i++) {
+                let grid = document.querySelector('.g-5')
+                let card = document.createElement('div');
+                card.className = 'col'
+                card.innerHTML = `
+            <div class="card h-100" style="width: 18rem;">
+                <img src="${json[i].imageUrl}" class="card-img-top h-100" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${json[i].name}</h5>
+                    <p class="card-text description fw-bold">${json[i].description}</p>
+                    <p class="card-text brand">${json[i].brand}</p>
+                    <p class="card-text price mt-auto">${json[i].price}€</p>
+                    <button type="button" class="btn btn-warning"><i
+                    class="bi bi-pencil-square"></i></button>
+            <button type="button" class="btn btn-danger mx-2 elimina"><i
+                    class="bi bi-trash-fill"></i></button>
+                </div>
+            </div>
+        `;
+                grid.appendChild(card);
+            }
         })
 }
 
-function createData(data, uri) {
-    fetch(uri, {
+function createData(data, url) {
+    fetch(url, {
         method: "POST",
         body: JSON.stringify(data),
         headers: {
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTc4MzU0OWMwNTgzNTAwMTg1MjJmODgiLCJpYXQiOjE3MDIzNzY3NzcsImV4cCI6MTcwMzU4NjM3N30.vNStGYea9QyMEVDBApMNADTJtNOZ7J0Hg6Z4ldzcaXQ',
+            Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTc4YWE0MDI2NzYxNDAwMTgzYzNiZDciLCJpYXQiOjE3MDI0MDY3MjUsImV4cCI6MTcwMzYxNjMyNX0.SaYsGxqmoTxMxi02CtqyfzZMi3PNHMeQG3ZlsBOHhn0",
             "Content-Type": "application/json"
         }
     })
         .catch(err => console.log(err))
 }
 
+function deleteData(productId) {
+    fetch(`https://striveschool-api.herokuapp.com/api/product/`, {
+        method: 'DELETE',
+        headers: {
+            Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTc4YWE0MDI2NzYxNDAwMTgzYzNiZDciLCJpYXQiOjE3MDI0MDY3MjUsImV4cCI6MTcwMzYxNjMyNX0.SaYsGxqmoTxMxi02CtqyfzZMi3PNHMeQG3ZlsBOHhn0",
+            "Content-Type": "application/json"
+        }
+    })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    let back = document.querySelector('.office');
+    back.addEventListener('click', () => {
+        window.location = 'backoffice.html'
+    })
+    let buttonDelete = document.querySelectorAll('.elimina');
+    buttonDelete.forEach(deleteButton => {
+        deleteButton.addEventListener('click', () => {
+        })
+    })
+    let cardId = document.querySelectorAll('.card-id');
+    for (let i = 0; i < cardId.length; i++) {
+        cardId[i].innerHTML = json[i]._id;
+    }
+})
 
 
 
